5473e286af0870bc453717476dd3f2a2f65ed1f6
Iman <imanali.cse@gmail.com>
2022-06-25 18:33:01 +0600

    encode decode

--- src/com/imanali/utils/CommonUtils.java
+++ src/com/imanali/utils/CommonUtils.java
@@ -39,6 +39,17 @@ public class CommonUtils {
         }
         return null;
     }
+
+    public static String encodeSimpleString(String text) {
+        String encodedString = Base64.getEncoder().encodeToString(text.getBytes());
+        return encodedString;
+    }
+
+    public static String decodeEncodedString(String encodedString) {
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        String decodedString = new String(decodedBytes);
+        return decodedString;
+    }
 /*
     public static Mail attachmentFiles(Mail mailInfo, String jsonString) {
         try {
