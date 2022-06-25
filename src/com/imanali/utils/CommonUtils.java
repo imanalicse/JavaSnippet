95f974bcb977a2805ef1948103ab675aa7469d27
Iman <imanali.cse@gmail.com>
2022-06-25 14:28:40 +0600

    common utils

--- src/com/imanali/utils/CommonUtils.java
+++ src/com/imanali/utils/CommonUtils.java
@@ -13,7 +13,32 @@ public class CommonUtils {
         String base64EncodedString = new String(encoded);
         return base64EncodedString;
     }
-
+/*
+    public static Mail attachmentFiles(Mail mailInfo, String jsonString) {
+        try {
+            if (!jsonString.isEmpty()) {
+                JSONArray attachmentsData = new JSONArray(jsonString);
+                int size = attachmentsData.length();
+                for (int i = 0; i < size; i++) {
+                    String filePath = attachmentsData.get(i).toString();
+                    File file = new File(filePath);
+                    if (file.exists()) {
+                        String fileName = file.getName();
+                        String fileContent = CommonUtils.convertFileToBase64(filePath);
+                        Attachments attachments = new Attachments();
+                        attachments.setFilename(fileName);
+                        attachments.setContent(fileContent);
+                        mailInfo.addAttachments(attachments);
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("File attachment exception: " + e.toString());
+        }
+        return mailInfo;
+    }
+*/
     public static boolean isNumeric(String str) {
         try {
             Double.parseDouble(str);
